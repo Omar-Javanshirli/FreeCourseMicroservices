55c3e66bc970ee6ccb57ced16b05f1234335cdd8
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-07-13 16:31:28 -0700

    i coded ICatalogService for Free.Course.Web

--- Frontends/FreeCourse.Web/Services/Interfaces/ICatalogService.cs
+++ Frontends/FreeCourse.Web/Services/Interfaces/ICatalogService.cs
@@ -9,17 +9,10 @@ namespace FreeCourse.Web.Services.Interfaces
     public interface ICatalogService
     {
         Task<List<CourseViewModel>> GetAllCourseAsync();
-
-        Task<List<CategoryViewModel>> GetAllCategoryAsync();
-
         Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId);
-
-        Task<CourseViewModel> GetByCourseId(string courseId);
-
-        Task<bool> CreateCourseAsync(CourseCreateInput courseCreateInput);
-
-        Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput);
-
+        Task<CourseViewModel> GetByCourseIdAsync(string courseId);
+        Task<bool> CreateCourseAsync(CourseCreateInput input);
+        Task<bool> UpdateCourseAsync(CourseUpdateInput input);
         Task<bool> DeleteCourseAsync(string courseId);
     }
 }
