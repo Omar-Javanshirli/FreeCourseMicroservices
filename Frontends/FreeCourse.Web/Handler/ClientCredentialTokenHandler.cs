eae6393c1cbbfc2f44edd63084f1a637c99ea24e
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-07-15 21:31:35 -0700

    i did implment ClientCredentialTokenHandler

--- Frontends/FreeCourse.Web/Handler/ClientCredentialTokenHandler.cs
+++ Frontends/FreeCourse.Web/Handler/ClientCredentialTokenHandler.cs
@@ -21,14 +21,13 @@ namespace FreeCourse.Web.Handler
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", await _clientCredentialTokenService.GetToken());
+            request.Headers.Authorization = new AuthenticationHeaderValue
+                ("Bearer", await _clientCredentialTokenService.GetToken());
 
             var response = await base.SendAsync(request, cancellationToken);
 
             if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-            {
                 throw new UnAuthorizeException();
-            }
 
             return response;
         }
