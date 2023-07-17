814a3bcf25a1f20e181a260e2f0f180bbe4cfb87
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-07-16 17:14:41 -0700

    i did implement Update and delete method in CourseController

--- Frontends/FreeCourse.Web/Controllers/CoursesController.cs
+++ Frontends/FreeCourse.Web/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
 ﻿using FreeCourse.Shared.Services;
+using FreeCourse.Web.Models.Catalogs;
 using FreeCourse.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -24,13 +25,68 @@ namespace FreeCourse.Web.Controllers
             return View(await _catalogService.GetAllCourseByUserIdAsync(_sharedIdentityService.GetUserId));
         }
 
+        [HttpGet]
         public async Task<IActionResult> Create()
         {
-            var categories=await this._catalogService.GetAllCategoryAsync();
-            ViewBag.categoryList = new SelectList(categories, "Id", "Name");
+            var categories = await _catalogService.GetAllCategoryAsync();
 
+            //Usere Name gosterilecek ama arxasinda Id deyerine tutaca, sonda ki iki parameter onun ucundur.
+            ViewBag.categoryList = new SelectList(categories, "Id", "Name");
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Create(CourseCreateInput courseCreateInput)
+        {
+            var categories = await _catalogService.GetAllCategoryAsync();
+            ViewBag.categoryList = new SelectList(categories, "Id", "Name");
+
+            if (ModelState is { IsValid: false })
+                return View();
+
+            courseCreateInput.UserId = _sharedIdentityService.GetUserId;
+            await _catalogService.CreateCourseAsync(courseCreateInput);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(string id)
+        {
+            var course = await _catalogService.GetByCourseId(id);
+            var categories = await _catalogService.GetAllCategoryAsync();
+
+
+            if (course == null)
+                RedirectToAction(nameof(Index));
+
+            ViewBag.categoryList = new SelectList(categories, "Id", "Name");
+
+            //Dto istifade etmek olardi
+            CourseUpdateInput courseUpdateInput = new()
+            {
+                Id = course.Id,
+                Name = course.Name,
+                Price = course.Price,
+                Description = course.Description,
+                Feature = course.Feature,
+                CategoryId = course.CategoryId,
+                UserId = course.UserId,
+                Picture = course.Picture
+            };
+
+            return View(courseUpdateInput);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(CourseUpdateInput courseUpdateInput)
+        {
+            var categories = await _catalogService.GetAllCategoryAsync();
+            ViewBag.categoryList = new SelectList(categories, "Id", "Name", courseUpdateInput.Id);
+
+            if (ModelState is { IsValid: false }) return View();
+
+            await _catalogService.UpdateCourseAsync(courseUpdateInput);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
