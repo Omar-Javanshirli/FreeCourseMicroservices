f39a3673de9d742333e7d5f7975ce988604b4c74
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-07-16 17:53:58 -0700

    i did impmelemetn IPhotoStockService/PhotoStockService

--- Frontends/FreeCourse.Web/Services/Interfaces/IPhotoStockService.cs
+++ Frontends/FreeCourse.Web/Services/Interfaces/IPhotoStockService.cs
@@ -10,7 +10,6 @@ namespace FreeCourse.Web.Services.Interfaces
     public interface IPhotoStockService
     {
         Task<PhotoViewModel> UploadPhoto(IFormFile photo);
-
         Task<bool> DeletePhoto(string photoUrl);
     }
 }
--- Frontends/FreeCourse.Web/Services/PhotoStockService.cs
+++ Frontends/FreeCourse.Web/Services/PhotoStockService.cs
@@ -21,39 +21,14 @@ namespace FreeCourse.Web.Services
             _httpClient = httpClient;
         }
 
-        public async Task<bool> DeletePhoto(string photoUrl)
+        public Task<bool> DeletePhoto(string photoUrl)
         {
-            var response = await _httpClient.DeleteAsync($"photos?photoUrl={photoUrl}");
-            return response.IsSuccessStatusCode;
+            throw new NotImplementedException();
         }
 
-        public async Task<PhotoViewModel> UploadPhoto(IFormFile photo)
+        public Task<PhotoViewModel> UploadPhoto(IFormFile photo)
         {
-            if (photo == null || photo.Length <= 0)
-            {
-                return null;
-            }
-            // örnek dosya ismi= 203802340234.jpg
-            var randonFilename = $"{Guid.NewGuid().ToString()}{Path.GetExtension(photo.FileName)}";
-
-            using var ms = new MemoryStream();
-
-            await photo.CopyToAsync(ms);
-
-            var multipartContent = new MultipartFormDataContent();
-
-            multipartContent.Add(new ByteArrayContent(ms.ToArray()), "photo", randonFilename);
-
-            var response = await _httpClient.PostAsync("photos", multipartContent);
-
-            if (!response.IsSuccessStatusCode)
-            {
-                return null;
-            }
-
-            var responseSuccess = await response.Content.ReadFromJsonAsync<Response<PhotoViewModel>>();
-
-            return responseSuccess.Data;
+            throw new NotImplementedException();
         }
     }
 }
