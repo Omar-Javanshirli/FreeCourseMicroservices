0f0818f5a4c93c4c641dfb8ea0165693f04a61b3
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-07-17 22:36:16 -0700

    i coded IBasketService

--- Frontends/FreeCourse.Web/Services/Interfaces/IBasketService.cs
+++ Frontends/FreeCourse.Web/Services/Interfaces/IBasketService.cs
@@ -1,7 +1,4 @@
 ﻿using FreeCourse.Web.Models.Baskets;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace FreeCourse.Web.Services.Interfaces
@@ -9,17 +6,11 @@ namespace FreeCourse.Web.Services.Interfaces
     public interface IBasketService
     {
         Task<bool> SaveOrUpdate(BasketViewModel basketViewModel);
-
         Task<BasketViewModel> Get();
-
         Task<bool> Delete();
-
         Task AddBasketItem(BasketItemViewModel basketItemViewModel);
-
         Task<bool> RemoveBasketItem(string courseId);
-
         Task<bool> ApplyDiscount(string discountCode);
-
         Task<bool> CancelApplyDiscount();
     }
 }
