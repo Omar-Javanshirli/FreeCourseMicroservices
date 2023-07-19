e130bae67e2d211bb5e5ddb7b5586d6b42870c9f
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-07-19 02:02:40 -0700

    Validator

--- Frontends/FreeCourse.Web/Validators/DiscountApplyInputValidator.cs
+++ Frontends/FreeCourse.Web/Validators/DiscountApplyInputValidator.cs
@@ -1,9 +1,5 @@
 ﻿using FluentValidation;
 using FreeCourse.Web.Models.Discounts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FreeCourse.Web.Validators
 {
