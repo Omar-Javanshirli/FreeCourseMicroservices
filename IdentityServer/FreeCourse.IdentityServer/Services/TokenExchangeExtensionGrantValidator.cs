081d4bfb271f1825ff99530c248a38eb162dfd66
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-08-10 17:33:21 -0700

    new update

--- IdentityServer/FreeCourse.IdentityServer/Services/TokenExchangeExtensionGrantValidator.cs
+++ IdentityServer/FreeCourse.IdentityServer/Services/TokenExchangeExtensionGrantValidator.cs
@@ -1,6 +1,4 @@
 ﻿using IdentityServer4.Validation;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +6,8 @@ namespace FreeCourse.IdentityServer.Services
 {
     public class TokenExchangeExtensionGrantValidator : IExtensionGrantValidator
     {
+        //istedeyiniz adalandirmani vere bilersiz
         public string GrantType => "urn:ietf:params:oauth:grant-type:token-exchange";
-
         private readonly ITokenValidator _tokenValidator;
 
         public TokenExchangeExtensionGrantValidator(ITokenValidator tokenValidator)
@@ -19,35 +17,43 @@ namespace FreeCourse.IdentityServer.Services
 
         public async Task ValidateAsync(ExtensionGrantValidationContext context)
         {
+            //requesti elde edirik.
             var requestRaw = context.Request.Raw.ToString();
 
+            //requestin tokenini elde edirik
             var token = context.Request.Raw.Get("subject_token");
 
+            //tokenin yoxlanmagi
             if (string.IsNullOrEmpty(token))
             {
-                context.Result = new GrantValidationResult(IdentityServer4.Models.TokenRequestErrors.InvalidRequest, "token missing");
+                context.Result = new GrantValidationResult
+                    (IdentityServer4.Models.TokenRequestErrors.InvalidRequest, "token missing");
                 return;
             }
 
+            //elde etdiyimiz tokeni dogruluyurux. Imzasi,vaxtinin bitim bitmediyini ve s.
             var tokenValidateResult = await _tokenValidator.ValidateAccessTokenAsync(token);
 
             if (tokenValidateResult.IsError)
             {
-                context.Result = new GrantValidationResult(IdentityServer4.Models.TokenRequestErrors.InvalidGrant, "token invalid");
-
+                context.Result = new GrantValidationResult
+                    (IdentityServer4.Models.TokenRequestErrors.InvalidGrant, "token invalid");
                 return;
             }
 
-            var subjectClaim = tokenValidateResult.Claims.FirstOrDefault(c => c.Type == "sub");
+            //tokenin paylod hissesinde itifadecinin id-sini saxladigi "sub" claimini elde edirik.
+            var subjectClaim = tokenValidateResult.Claims.FirstOrDefault(x => x.Type == "sub");
 
             if (subjectClaim == null)
             {
-                context.Result = new GrantValidationResult(IdentityServer4.Models.TokenRequestErrors.InvalidGrant, "token must contain sub value");
-
+                context.Result = new GrantValidationResult
+                    (IdentityServer4.Models.TokenRequestErrors.InvalidRequest, "token must contain sub value");
                 return;
             }
 
-            context.Result = new GrantValidationResult(subjectClaim.Value, "access_token", tokenValidateResult.Claims);
+            //istifadecinin id-sini, access token oldugunu bildirirk ve tokeninin icinde ki claimlari gonderirik. 
+            context.Result = new GrantValidationResult(subjectClaim.Value, "access_token",
+               tokenValidateResult.Claims);
 
             return;
         }
--- IdentityServer/FreeCourse.IdentityServer/Startup.cs
+++ IdentityServer/FreeCourse.IdentityServer/Startup.cs
@@ -1,9 +1,7 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-using IdentityServer4;
-using FreeCourse.IdentityServer.Data;
+﻿using FreeCourse.IdentityServer.Data;
 using FreeCourse.IdentityServer.Models;
+using FreeCourse.IdentityServer.Services;
+using IdentityServer4;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -11,7 +9,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using FreeCourse.IdentityServer.Services;
 
 namespace FreeCourse.IdentityServer
 {
@@ -26,7 +23,7 @@ namespace FreeCourse.IdentityServer
             Configuration = configuration;
         }
 
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services) 
         {
             services.AddLocalApiAuthentication();
 
